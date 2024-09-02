b7fac035749a07aeacbe19073240a643118fe0a7
Av1sF <88183334+Av1sF@users.noreply.github.com>
2024-09-02 11:20:15 +0100

    implemented graph

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // defining initial state of the graph is hidden
+      showGraph: false,
     };
   }
 
@@ -29,7 +32,11 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+  // if user has clicked the 'Start Streaming' button
+    if (this.state.showGraph) {
+        // show graph
+        return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -37,9 +44,23 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      let count = 0;
+      // how often new data is retrieved from server
+      const delay = 1000 //ms ;
+
+      const interval = setInterval(() => {
+      // when server responds assign these variables
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
+            this.setState({
+                data: serverResponds,
+                showGraph: true,
+            });
+        });
+        count++;
+        if (count > delay) {
+            clearInterval(interval);
+        }
+      }, 100);
     });
   }
 
